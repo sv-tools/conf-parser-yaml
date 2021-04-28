4bfb03e5582f66a7809a5d7a61ff625f4fe87f01
Sergey Vilgelm <sergey@vilgelm.com>
2021-04-28 07:06:01 -0500

    The Code (#1)

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module github.com/sv-tools/go-repo-template
+module github.com/sv-tools/conf-parser-yaml
 
 go 1.16
+
+require (
+	github.com/stretchr/testify v1.7.0
+	github.com/sv-tools/conf v0.1.1
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
---
+++ parser_test.go
@@ -0,0 +1,58 @@
+package confyaml_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/sv-tools/conf"
+
+	confyaml "github.com/sv-tools/conf-parser-yaml"
+)
+
+const data = `
+foo: 42
+bar: test
+`
+
+const wrongData = `
+foo: 42
+- bar: test
+`
+
+func TestParser(t *testing.T) {
+	c := conf.New().WithReaders(conf.NewStreamParser(strings.NewReader(data)).WithParser(confyaml.Parser))
+	require.NoError(t, c.Load(context.Background()))
+
+	require.Equal(t, 42, c.GetInt("foo"))
+	require.Equal(t, "test", c.Get("bar"))
+}
+
+var errFake = errors.New("fake error")
+
+type testReader struct{}
+
+func (t *testReader) Read(_ []byte) (int, error) {
+	return 0, errFake
+}
+
+func TestParserErrors(t *testing.T) {
+	c := conf.New().WithReaders(conf.NewStreamParser(&testReader{}).WithParser(confyaml.Parser))
+	require.ErrorIs(t, c.Load(context.Background()), errFake)
+
+	c = conf.New().WithReaders(conf.NewStreamParser(strings.NewReader(wrongData)).WithParser(confyaml.Parser))
+	require.EqualError(t, c.Load(context.Background()), "yaml: line 1: did not find expected key")
+}
+
+func ExampleParser() {
+	c := conf.New().WithReaders(conf.NewStreamParser(strings.NewReader(data)).WithParser(confyaml.Parser))
+	if err := c.Load(context.Background()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(c.GetInt("foo"))
+	// Output: 42
+}
